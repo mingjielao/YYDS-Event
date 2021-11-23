4a76018259061aa7a979c5999eb80d1eb82928da
jiexi <mingjielao@gmail.com>
2021-11-23 18:10:21 -0500

    security

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
-from flask import Flask, Response, request
+import os
+from flask import Flask, Response, request, redirect, url_for, session
 from flask_cors import CORS
 import json
 import logging
 
 from utils.rest_utils import RESTContext
 from middleware.service_factory import ServiceFactory
-
+from flask_dance.contrib.google import make_google_blueprint, google
+import middleware.security as security
+from middleware.notification import NotificationMiddlewareHandler
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 app = Flask(__name__)
 CORS(app)
+
+client_id = "760166083721-g03if15thd5a5i1ceeuqkr60m729o8pl.apps.googleusercontent.com"
+client_secret = "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"
+app.secret_key = "some secret"
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+blueprint = make_google_blueprint(
+    client_id=client_id,
+    client_secret=client_secret,
+    reprompt_consent=True,
+    scope=["profile", "email"],
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+g_bp = app.blueprints.get("google")
+
+
+@app.before_request
+def before_request_func():
+    result_ok = security.check_security(request, google, g_bp)
+    print("before request...")
+    if not result_ok:
+        return redirect(url_for("google.login"))
+
+
+@app.after_request
+def after_request_func(response):
+    print("after_request is running!")
+    return response
 
 
 @app.route('/')
@@ -21,12 +54,14 @@ def hello_world():
 
 @app.route('/api/<resource_collection>', methods=["GET", "POST"])
 def do_resource_collection(resource_collection):
-
     request_inputs = RESTContext(request, resource_collection)
     service = ServiceFactory()
     svc = service.get_service(resource_collection)
 
-    if request_inputs.method == "GET":
+    if svc is None:
+        rsp = Response(status=400, content_type="application/json")
+
+    elif request_inputs.method == "GET":
         res = svc.get_by_template(request_inputs.args,
                                   field_list=request_inputs.fields,
                                   limit=request_inputs.limit,
@@ -36,7 +71,7 @@ def do_resource_collection(resource_collection):
 
     elif request_inputs.method == "POST":
         res = svc.create(request.get_json())
-        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        rsp = Response(json.dumps(res, default=str), status=201, content_type="application/json")
 
     return rsp
 
@@ -46,8 +81,9 @@ def specific_resource(resource_collection, resource_id):
     request_inputs = RESTContext(request, resource_collection)
     service = ServiceFactory()
     svc = service.get_service(resource_collection)
-
-    if request_inputs.method == "GET":
+    if svc is None:
+        rsp = Response(status=400, content_type="application/json")
+    elif request_inputs.method == "GET":
         res = svc.get_by_resource_id(resource_id)
         rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
     elif request_inputs.method == "PUT":
@@ -55,7 +91,7 @@ def specific_resource(resource_collection, resource_id):
         rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
     elif request_inputs.method == "DELETE":
         res = svc.delete_by_resource_id(resource_id)
-        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        rsp = Response(json.dumps(res, default=str), status=204, content_type="application/json")
     return rsp
 
 
